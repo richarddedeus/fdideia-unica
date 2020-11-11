eeaada378355b2f09ae041fa96e4deb1fac36a07
Richard de Deus <richard.deus@gmail.com>
2020-11-11 09:56:49 -0300

    nova chamada de home

--- pages/index.js
+++ pages/index.js
@@ -3,26 +3,11 @@ import { useState } from "react"
 function Home(){
     return (
         <div>
-            <h1>Home</h1>
-            <Contador />
+            <h1>Página Inicial</h1>
+           
         </div>
     )
 }
 
-function Contador() {
-    const [contador, setContador] = useState(1);
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
-    return (
-        <div>
-            <div>
-                {contador}
-                <button onClick={adicionarContador}>Adicionar</button>
-            </div>
-        
-        </div>
-    )
-}
 
 export default Home
