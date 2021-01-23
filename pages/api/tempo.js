5bd62095b7af8670b76d70eed5ac27f3323c733f
Richard de Deus <richard.deus@gmail.com>
2021-01-22 21:25:06 -0300

    atualizando

---
+++ pages/api/tempo.js
@@ -0,0 +1,3 @@
+function tempo(request, response){
+    
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
-import { useState } from "react"
+import Link from 'next/link';
+import link from 'next/link';
 
 function Home(){
     return (
         <div>
             <h1>Página Inicial</h1>
            
+            <Link href="/sobre">
+                <a>Acessar página sobre</a>
+            </Link>
         </div>
     )
 }
